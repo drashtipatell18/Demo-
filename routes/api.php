cd7db8a8b3cf5105c0f86400e0b74989588e04b5
Dasu <drashtii.kalathiyainfotech@gmail.com>
2026-03-02 16:55:16 +0530

    confic solve

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Validator;
 
         Route::post('auth/logout', [GoogleAuthController::class, 'logout']);
         Route::post('auth/update-profile', [GoogleAuthController::class, 'updateProfile']);
-<<<<<<< Updated upstream
         Route::get('/threads', [ThreadController::class, 'index']);          // Get all threads
         Route::post('/threads', [ThreadController::class, 'store']);         // Create thread + first message
         Route::get('/threads/{id}', [ThreadController::class, 'show']);      // Get single thread messages
@@ -20,16 +19,7 @@ use Illuminate\Support\Facades\Validator;
         Route::post('/threads/{id}/message', [ThreadController::class, 'sendMessage']);     // user message
         Route::post('/threads/{id}/response', [ThreadController::class, 'storeResponse']); // model response
     });
-=======
-        Route::get('/threads', [ThreadController::class, 'index']);          
-        Route::post('/threads', [ThreadController::class, 'store']);         
-        Route::get('/threads/{id}', [ThreadController::class, 'show']);     
-        Route::put('/threads/{id}', [ThreadController::class, 'update']);   
-        Route::delete('/threads/{id}', [ThreadController::class, 'destroy']); 
-        Route::post('/messages/send', [ThreadController::class, 'sendMessage']);    
-        Route::post('/threads/{id}/response', [ThreadController::class, 'storeResponse']);
-    });     
->>>>>>> Stashed changes
+
     Route::post('/contact', [ContactFormController::class, 'store']);
     Route::get('/get-contact', [ContactFormController::class, 'GetContact']);
     Route::post('/auth/login', [GoogleAuthController::class, 'login']);
